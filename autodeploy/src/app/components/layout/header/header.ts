db4f2bc0e0258c56caf9410b8b84c2d96c5cb0aa
Kruhale <abravoc@iesrafaelalberti.es>
2026-04-22 20:06:11 +0200

    feat(#158): añadir ThemeService y toggle dark/light mode en cabecera

--- autodeploy/src/app/components/layout/header/header.ts
+++ autodeploy/src/app/components/layout/header/header.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { AuthService } from "../../../services/auth.service";
+import { ThemeService } from "../../../services/theme.service";
 
 @Component({
   selector: "app-header",
@@ -9,5 +10,8 @@ import { AuthService } from "../../../services/auth.service";
   styleUrl: "./header.scss",
 })
 export class Header {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    public themeService: ThemeService,
+  ) {}
 }

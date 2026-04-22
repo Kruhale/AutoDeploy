929688028939dbe1d5df91ccdd0a9d0a55917ce4
Kruhale <abravoc@iesrafaelalberti.es>
2026-04-22 20:20:37 +0200

    feat(#172): añadir páginas legales, banner de cookies y enlaces en footer

--- autodeploy/src/app/components/layout/landing-layout/landing-layout.ts
+++ autodeploy/src/app/components/layout/landing-layout/landing-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { BannerCookies } from '../../banner-cookies/banner-cookies';
 
 @Component({
   selector: 'app-landing-layout',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, BannerCookies],
   templateUrl: './landing-layout.html',
   styleUrl: './landing-layout.scss'
 })

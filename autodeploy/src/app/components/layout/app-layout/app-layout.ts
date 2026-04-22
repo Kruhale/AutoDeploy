54b80c218dd2d99e82dc87057a0a65ffb7d097ee
Kruhale <abravoc@iesrafaelalberti.es>
2026-04-22 20:19:21 +0200

    feat(#160): sidebar responsive con menú hamburguesa y overlay en móvil

--- autodeploy/src/app/components/layout/app-layout/app-layout.ts
+++ autodeploy/src/app/components/layout/app-layout/app-layout.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar.component';
 import { BarraPieApp } from '../barra-pie-app/barra-pie-app';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [RouterOutlet, Sidebar, BarraPieApp],
+  imports: [RouterOutlet, RouterLink, Sidebar, BarraPieApp],
   templateUrl: './app-layout.html',
   styleUrl: './app-layout.scss'
 })
-export class AppLayout {}
+export class AppLayout {
+  sidebarAbierta = signal(false);
+
+  alternarSidebar(): void {
+    this.sidebarAbierta.update(v => !v);
+  }
+
+  cerrarSidebar(): void {
+    this.sidebarAbierta.set(false);
+  }
+}
